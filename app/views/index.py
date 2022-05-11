7cc9a44bbf04a456a4c299e81fd836c09f775697
chick_0 <chick_0@ch1ck.xyz>
2022-05-11 15:36:33 +0900

    create index page

--- app/views/index.py
+++ app/views/index.py
@@ -1,3 +1,11 @@
 from flask import Blueprint
+from flask import render_template
 
 bp = Blueprint("index", __name__, url_prefix="/")
+
+
+@bp.get("")
+def index():
+    return render_template(
+        "index/index.html"
+    )
