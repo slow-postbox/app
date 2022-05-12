cd1a0a7f0660ebc0a7f0a00d8b7e1299b918a274
chick_0 <chick_0@ch1ck.xyz>
2022-05-13 00:07:42 +0900

    create dashboard

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -2,6 +2,12 @@ from flask import Blueprint
 from flask import session
 from flask import redirect
 from flask import url_for
+from flask import render_template
+
+from app import db
+from app.models import User
+from app.models import LoginHistory
+from app.models import Mail
 
 bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 
@@ -11,4 +17,31 @@ def index():
     if 'user' not in session:
         return redirect(url_for("auth.login"))
 
-    return "WIP"
+    user = User.query.filter_by(
+        id=session['user']['user_id']
+    ).first()
+
+    if user is None:
+        del session['user']
+        return redirect(url_for("auth.sign_up", error="삭제된 계정입니다."))
+
+    mails = Mail.query.filter_by(
+        owner_id=user.id
+    ).all()
+
+    return render_template(
+        "dashboard/index.html",
+        user=user,
+        mails=mails,
+        mail_count=len(mails),
+    )
+
+
+@bp.get("/history")
+def history():
+    return
+
+
+@bp.get("/quit-service")
+def quit_service():
+    return
