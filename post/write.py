c5c9c0bddb2e3678e0ae5b2bdbd2198a298f501a
chick_0 <chick_0@ch1ck.xyz>
2022-05-13 00:37:15 +0900

    create mail editor page

--- post/write.py
+++ post/write.py
@@ -1,11 +1,24 @@
+from datetime import datetime
+from datetime import timedelta
+
 from flask import Blueprint
+from flask import g
+from flask import session
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask import render_template
 
 bp = Blueprint("write", __name__, url_prefix="/write")
 
 
 @bp.get("/create-new")
 def create_new():
-    return "create_new"
+    g.editor_css = True
+    return render_template(
+        "post/write/create-new.html",
+        date=(datetime.now() + timedelta(days=365)).strftime("%Y-%m-%d")
+    )
 
 
 @bp.post("/create-new")
@@ -15,7 +28,10 @@ def create_new_post():
 
 @bp.get("/<int:mail_id>")
 def edit(mail_id: int):
-    return f"{mail_id}"
+    g.editor_css = True
+    return render_template(
+        "post/write/edit.html",
+    )
 
 
 @bp.post("/<int:mail_id>")
