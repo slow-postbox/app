289760c471139cbe66565ffe62df77d80df2cccb
chick_0 <chick_0@ch1ck.xyz>
2022-05-13 00:13:47 +0900

    setup url route

--- post/write.py
+++ post/write.py
@@ -1,3 +1,23 @@
 from flask import Blueprint
 
 bp = Blueprint("write", __name__, url_prefix="/write")
+
+
+@bp.get("/create-new")
+def create_new():
+    return "create_new"
+
+
+@bp.post("/create-new")
+def create_new_post():
+    return "create_new"
+
+
+@bp.get("/<int:mail_id>")
+def edit(mail_id: int):
+    return f"{mail_id}"
+
+
+@bp.post("/<int:mail_id>")
+def edit_post(mail_id: int):
+    return f"{mail_id}"
