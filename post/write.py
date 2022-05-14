939b6bb0558c4f1e1c58337df173850b52aeb710
chick_0 <chick_0@ch1ck.xyz>
2022-05-14 20:41:41 +0900

    create save button

--- post/write.py
+++ post/write.py
@@ -30,26 +30,55 @@ def create_new(user: User):
 @bp.post("/create-new")
 @login_required
 def create_new_post(user: User):
+    def error(message):
+        return redirect(url_for("post.write.create_new", error=message))
+
+    try:
+        content = request.form['content']
+
+        if len(content) > 20000:
+            return error("본문이 20000자 보다 길 수 없습니다.")
+    except KeyError:
+        return error("편지 본문을 전달 받지 못 했습니다.")
+
+    try:
+        # 편지를 보낼 날짜를 불러옴
+        send_date = datetime.strptime(request.form['date'], "%Y-%m-%d")
+    except KeyError:
+        # 기본 값은 작성일로부터 1년뒤
+        send_date = (datetime.now() + timedelta(days=365)).strftime("%Y-%m-%d")
+
+    print(
+        send_date - datetime.now(),
+        send_date - datetime.now() > timedelta(days=365 + 15),
+        send_date > datetime.now(),
+        send_date < datetime.now(),
+    )
+
     mail = Mail()
     mail.owner_id = user.id
-    mail.send_date = datetime.strptime(request.form['date'], "%Y-%m-%d")
-    mail.title = request.form['title']
-    mail.content = request.form['content']
-    mail.lock = False
+    mail.send_date = send_date
+    mail.title = request.form.get("title", "제목 없는 편지").strip()[:100]
+    mail.content = content
+    mail.lock = True if request.form.get("lock", "false") == 'true' else False
 
     db.session.add(mail)
     db.session.commit()
 
-    return redirect(url_for("mail.edit", mail_id=mail.id))
+    return redirect(url_for("post.write.edit", mail_id=mail.id))
 
 
 @bp.get("/<int:mail_id>")
 @login_required
 @fetch_mail
 def edit(user: User, mail: Mail, mail_id: int):
+    if mail.lock is True:
+        return "해당 편지는 수정할 수 없습니다."
+
     g.editor_css = True
     return render_template(
         "post/write/edit.html",
+        m=mail
     )
 
 
@@ -57,5 +86,8 @@ def edit(user: User, mail: Mail, mail_id: int):
 @login_required
 @fetch_mail
 def edit_post(user: User, mail: Mail, mail_id: int):
+    if mail.lock is True:
+        return "해당 편지는 수정할 수 없습니다."
+
     print(mail_id, user, mail)
     return "WIP : check console"
