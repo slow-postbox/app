735685b907e8d03cd8101ecc547311c1429cb019
chick_0 <chick_0@ch1ck.xyz>
2022-05-17 00:33:33 +0900

    check history for verify session

--- app/utils.py
+++ app/utils.py
@@ -7,6 +7,7 @@ from flask import redirect
 from flask import url_for
 
 from app.models import User
+from app.models import LoginHistory
 from app.models import Mail
 
 
@@ -47,6 +48,18 @@ def login_required(f):
             del session['user']
             error_id = set_error_message(message="삭제된 계정입니다.")
             return redirect(url_for("auth.sign_up", error=error_id))
+
+        history = LoginHistory.query.with_entities(
+            LoginHistory.id
+        ).filter_by(
+            id=session['user']['history_id'],
+            owner_id=user_id,
+        ).first()
+
+        if history is None:
+            del session['user']
+            error_id = set_error_message(message="로그인 기록이 없는 세션입니다.")
+            return redirect(url_for("auth.login", error=error_id))
 
         kwargs.update({"user": user})
         return f(*args, **kwargs)
