dc45281bc6554edcceb0f4f8cf0ab0288734c299
chick_0 <chick_0@ch1ck.xyz>
2022-06-12 01:43:48 +0900

    remove unused func

--- mail/send.py
+++ mail/send.py
@@ -62,27 +62,3 @@ def send_password_reset(email: str, url: str):
             to_addrs=[email],
             msg=payload.as_string()
         )
-
-
-def send_mail(email: str, url: str, title: str):
-    html = "\n".join([
-        f"<p><a href=\"{url}\" target=\"_blank\">다음 링크</a>로 접속해 편지를 확인 해주세요.</p>",
-        "<p>* 일정 시간이후 편지가 삭제되기 때문에 인쇄하기 기능을 통해 편지를 보관해주세요.</p>"
-    ])
-
-    payload = MIMEText(html, "html", "utf-8")
-    payload['From'] = SMTP_USER
-    payload['Subject'] = f"{environ['TITLE']} - {title}"
-
-    with SMTP(SMTP_HOST, SMTP_PORT) as client:
-        client.starttls()
-        client.login(
-            user=SMTP_USER,
-            password=SMTP_PASSWORD
-        )
-
-        client.sendmail(
-            from_addr=SMTP_USER,
-            to_addrs=[email],
-            msg=payload.as_string()
-        )
