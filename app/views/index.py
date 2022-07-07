3d5851987b4000096b168e28afa09854fd944b89
chick_0 <chick_0@ch1ck.xyz>
2022-07-07 20:30:47 +0900

    add login block in index

--- app/views/index.py
+++ app/views/index.py
@@ -1,10 +1,13 @@
 from flask import Blueprint
 from flask import render_template
 
+from app.utils import login_block
+
 bp = Blueprint("index", __name__, url_prefix="/")
 
 
 @bp.get("")
+@login_block
 def index():
     return render_template(
         "index/index.html"
